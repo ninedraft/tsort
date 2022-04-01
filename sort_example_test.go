b3cca6f54eada84052027756abf32997432cabdc
merlin <clamores.pro@gmail.com>
2022-04-01 19:28:35 +0300

    adding example test

---
+++ sort_example_test.go
@@ -0,0 +1,24 @@
+package tsort_test
+
+import (
+	"fmt"
+
+	"github.com/ninedraft/tsort"
+)
+
+func ExampleSort() {
+	var graph = [][]int{
+		0: {},
+		1: {0, 2},
+		2: {0},
+		3: {1, 2},
+	}
+
+	sorted, hasCycle := tsort.Sort(
+		[]int{0, 1, 2, 3},
+		func(i int) []int {
+			return graph[i]
+		})
+	fmt.Println(sorted, hasCycle)
+	// Output: [3 1 2 0] false
+}
